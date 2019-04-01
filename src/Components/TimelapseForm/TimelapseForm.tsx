82bfe56750f0cdae7832e1aa4eb3362c4cc8a278
zdwolfe <wolfe.zach@gmail.com>
2019-03-31 19:36:32 -0700

    Add defaults

--- src/Components/TimelapseForm/TimelapseForm.tsx
+++ src/Components/TimelapseForm/TimelapseForm.tsx
@@ -11,7 +11,7 @@ class TimelapseFormImpl extends React.Component<ITimelapseFormProps> {
   public render() {
     const { getFieldDecorator } = this.props.form;
 
-    const timelapseDurationUnitSelector = getFieldDecorator('durationUnit', {
+    const timelapseDurationUnitSelector = getFieldDecorator('timelapseDurationUnit', {
       initialValue: 'seconds',
     })(
       <Select>
@@ -48,6 +48,7 @@ class TimelapseFormImpl extends React.Component<ITimelapseFormProps> {
       <Form labelCol={{ span: 8 }} wrapperCol={{ span: 14 }} onSubmit={this.handleSubmit}>
         <Form.Item label={"Video Duration"}>
           {getFieldDecorator('timelapseDuration', {
+            initialValue: 10,
             rules: [{ required: false, message: 'Please input the expected timelapse length.' }],
           })(
             <Input autoFocus={true} addonAfter={timelapseDurationUnitSelector} style={{ width: '100%' }} />
@@ -56,6 +57,7 @@ class TimelapseFormImpl extends React.Component<ITimelapseFormProps> {
 
         <Form.Item label={"Capture Duration"}>
           {getFieldDecorator('recordingDuration', {
+            initialValue: 30,
             rules: [{ required: false, message: 'Please input the expected recording time.' }],
           })(
             <Input addonAfter={recordingDurationUnitSelector} style={{ width: '100%' }} />
@@ -64,6 +66,7 @@ class TimelapseFormImpl extends React.Component<ITimelapseFormProps> {
 
         <Form.Item label={"Frames / sec"}>
           {getFieldDecorator('fps', {
+            initialValue: 30,
             rules: [{ required: false, message: 'Please input the expected fps.' }],
           })(
             <Input style={{ width: '100%' }}  />
@@ -103,11 +106,10 @@ class TimelapseFormImpl extends React.Component<ITimelapseFormProps> {
 
     const formTimelapseDuration = this.props.form.getFieldValue("timelapseDuration");
     const formTimelapseDurationUnit = this.props.form.getFieldValue("timelapseDurationUnit");
-    const formRecordingDuration = this.props.form.getFieldValue("RecordingDuration");
-    const formRecordingDurationUnit = this.props.form.getFieldValue("RecordingDurationUnit");
+    const formRecordingDuration = this.props.form.getFieldValue("recordingDuration");
+    const formRecordingDurationUnit = this.props.form.getFieldValue("recordingDurationUnit");
     const formFps = this.props.form.getFieldValue("fps");
     const formIntervalSec = this.props.form.getFieldValue("intervalSec");
-
     console.log(
       `
       formTimelapseDuration=${formTimelapseDuration}
@@ -118,6 +120,9 @@ class TimelapseFormImpl extends React.Component<ITimelapseFormProps> {
       formIntervalSec=${formIntervalSec}
       `
     );
+
+
+    
   }
 }
 
